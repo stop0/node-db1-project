92b108193abc1a5a616720e82cd8bc6675671ac5
MRSTOP0\Stop0 <dawodmohmmed@gmail.com>
2021-03-16 02:51:24 -0400

    almost

--- api/server.js
+++ api/server.js
@@ -1,7 +1,21 @@
 const express = require("express");
 
 const server = express();
+const accountsRouter = require("./accounts/accounts-router");
 
 server.use(express.json());
+
+
+server.use("/api/accounts", accountsRouter)
+
+
+
+server.get("/", (req, res) => {
+    res.json({ message: "Server is connected!" });
+  });
+
+
+
+
 
 module.exports = server;
